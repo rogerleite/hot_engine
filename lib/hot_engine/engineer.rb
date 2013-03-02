1214d33af150c7f2e71e36b4dba9a78157cc92ef
Roger Leite <roger.barreto@gmail.com>
2013-03-02 01:03:44 -0300

    Removing useless finders

--- lib/hot_engine/engineer.rb
+++ lib/hot_engine/engineer.rb
@@ -8,17 +8,6 @@ module HotEngine
       end
     end
 
-    def self.find(engine_id)
-      engine = self.find!(engine_id)
-      raise ArgumentError, "Engine #{engine_id} not found" if engine.nil?
-      engine
-    end
-
-    def self.find!(engine_id)
-      engine_id = engine_id.to_i
-      all.detect { |engine| engine.id == engine_id }
-    end
-
     def self.find_by_name(name)
       name = name.to_s
       engine = all.detect { |engine| engine.name == name }
